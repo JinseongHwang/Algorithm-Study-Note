8613ae7796ec3bf4493e27aae24ce6954a984b45
JinseongHwang <eddy5360@naver.com>
2020-08-14 01:52:53 +0900

    최단경로 - 다익스트라 알고리즘

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -11,49 +11,41 @@
 	시간복잡도 : (우선순위 큐를 사용할 경우) O(E log V)
 
 	참고: https://blog.naver.com/kks227/220796029558
-
 */
 
 // #1753 최단경로
 
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <algorithm>
-#include <functional>
 #include <utility>
+#include <queue>
+#include <functional>
+#include <algorithm>
 using namespace std;
 
-typedef pair<int, int> pii; // 가중치, 정점 번호
+typedef pair<int, int> pii;
 const int MAX = 2e4 + 10, INF = 0x7fffffff;
-int V, E, S, dist[MAX];
-bool visited[MAX];
-vector<pii> graph[MAX];
-priority_queue<pii, vector<pii>, greater<pii> > pq; // 최소힙
+int V, E, K; // V: 정점의 개수, E: 간선의 개수, K: 시작 정점 번호
+int dist[MAX]; // dist[i]: 시작 정점으로부터 i번째 정점까지의 최단 거리
+vector<pii> edge[MAX]; // first: 정점 번호, second: 비용
+priority_queue<pii, vector<pii>, greater<pii> > pq; // 최소힙 - first: 비용, second: 정점 번호
 
-void dijkstra(int start) {
-	dist[start] = 0; // 시작 정점으로의 거리는 0이다
-	pq.push(pii(0, start)); // 시작 정점만 넣고 시작한다
+void dijkstra() {
+	dist[K] = 0; // 시작 정점으로의 거리는 0이다
+	pq.push(pii(0, K)); // 시작 정점의 정보를 pq에 삽입
 
-	while (!pq.empty()) {
-		int curr;
-		do {
-			curr = pq.top().second;
-			pq.pop();
-		// pq가 비어있지 않고, curr이 방문한 상태라면 계속 반복
-		// pq가 비어있거나, curr이 방문하지 않았다면 break
-		} while (!pq.empty() && visited[curr]);
-		
-		// 이미 방문한 정점들밖에 없어서 curr마저 방문된 상태라면 break
-		if (visited[curr]) break;
-		visited[curr] = true; // 방문 체크
-
-		for (const auto& nxt : graph[curr]) {
-			// first: 가중치 / second: 정점 번호
-			// 경유해서 가는 것이 더 짧은 거리를 가지면 갱신하고 push한다
-			if (dist[nxt.second] > dist[curr] + nxt.first) {
-				dist[nxt.second] = dist[curr] + nxt.first;
-				pq.push(pii(dist[nxt.second], nxt.second));
+	while (!pq.empty()) { // pq가 비었을 경우 종료한다
+		int cost = pq.top().first;
+		int curr = pq.top().second;
+		pq.pop();
+		if (dist[curr] < cost) continue; // 갱신될 가능성이 없는 경우
+		for (const auto& elem : edge[curr]) { // curr에서 갈 수 있는 간선들을 살펴봄
+			int nxt = elem.first;
+			int ncost = elem.second;
+			// 만약 현재 저장된 거리값이 curr을 경유해서 가는 것보다 클 경우 갱신한다
+			if (dist[nxt] > cost + ncost) {
+				dist[nxt] = cost + ncost;
+				pq.push(pii(dist[nxt], nxt));
 			}
 		}
 	}
@@ -63,21 +55,18 @@ int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	// 최초 dist 배열을 INF값으로 초기화시켜준다.
-	fill(dist, dist + MAX, INF);
-
-	cin >> V >> E >> S;
-	// 정점의 개수, 간선의 개수, 시작 정점
-	for (int u, v, w, i = 0; i < E; ++i) {
-		cin >> u >> v >> w; // from, to, cost
-		graph[u].push_back(pii(w, v)); // cost, to 삽입
+	cin >> V >> E >> K;
+	// 최초 dist 배열을 INF값으로 초기화한다.
+	fill(dist, dist + V + 1, INF);
+	// 간선의 개수만큼 반복 후 edge 벡터에 저장
+	for (int from, to, cost, i = 0; i < E; ++i) {
+		cin >> from >> to >> cost;
+		edge[from].push_back(pii(to, cost));
 	}
-	dijkstra(S); // 다익스트라 알고리즘 실행
-
-	for (int i = 1; i <= V; ++i) { // dist 배열 출력
+	dijkstra(); // 다익스트라 알고리즘 실행
+	for (int i = 1; i <= V; ++i) { // 출력
 		if (dist[i] == INF) cout << "INF\n";
 		else cout << dist[i] << '\n';
 	}
-
 	return 0;
 }
