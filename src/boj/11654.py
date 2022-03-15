468c912cf397fd107dc66a289214b9a8fc622307
Jinseong Hwang <jinseong.dev@gmail.com>
2022-03-15 16:13:07 +0900

    아스키 코드

---
+++ src/boj/11654.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+"""
+ASCII char to order -> ord()
+ex) ord('A') == 65
+
+ASCII order to char -> chr()
+ex) chr(65) == 'A'
+"""
+
+print(ord(input().rstrip()))
